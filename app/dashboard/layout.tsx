17713db3789009e7010a3ebf89d696cbf0145ddc
Naafiul Razzaq <naafiulrazzaq@gmail.com>
2023-11-24 17:50:22 +0700

    fitur edit data dan tabel transaksi

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,47 +1,68 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
+import Image from "next/image";
+import Link from "next/link";
+import ButtonLogout from "../_components/buttonlogout";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
-import Image from 'next/image';
-import Link from 'next/link';
-import ButtonLogout from '../_components/buttonlogout';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const logo = 'https://flowbite.com/docs/images/logo.svg';
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const logo = "https://flowbite.com/docs/images/logo.svg";
 
   return (
     <>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-              <p className="self-center text-2xl font-thin text-gray-600 px-[100px]">Member</p>
+          <p className="self-center text-2xl font-thin text-gray-600 px-[100px]">
+            Member
+          </p>
           <label htmlFor="my-drawer-2">
-            <FontAwesomeIcon icon={faBars} className="btn drawer-button lg:hidden" size="sm" />
+            <FontAwesomeIcon
+              icon={faBars}
+              className="btn drawer-button lg:hidden"
+              size="sm"
+            />
           </label>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <div className="">
                 <li className="mr-[40px]">
-                  <ButtonLogout/>
+                  <ButtonLogout />
                 </li>
               </div>
             </ul>
           </div>
         </div>
       </nav>
-      <aside id="default-sidebar" className=" fixed top-0 left-0 z-40 w-20 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+      <aside
+        id="default-sidebar"
+        className=" fixed top-0 left-0 z-40 w-20 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+      >
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
-            <li className='flex justify-center'>
-            <svg width="30" height="30" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" >
-              <path
-                d="M43.7839 54.579L78.775 98.4332C79.6625 99.5013 80.9969 99.995 82.5957 99.995H95.5624C98.0487 99.995 100 98.4332 100 96.4433V3.54585C100 0.205776 94.763 -1.28542 92.006 1.34432L43.7839 50.317C42.7201 51.4555 42.7201 53.2289 43.7839 54.579ZM4.44326 100H27.5315C30.0178 100 31.9691 98.4383 31.9691 96.4483V72.8461C31.9691 69.5061 26.7321 68.0149 23.9751 70.6446L0.886871 94.2418C-1.32879 96.6599 0.88687 100 4.44326 100Z"
-                fill="#5C5CFF"
-              />
-            </svg>
+            <li className="flex justify-center">
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 100 100"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M43.7839 54.579L78.775 98.4332C79.6625 99.5013 80.9969 99.995 82.5957 99.995H95.5624C98.0487 99.995 100 98.4332 100 96.4433V3.54585C100 0.205776 94.763 -1.28542 92.006 1.34432L43.7839 50.317C42.7201 51.4555 42.7201 53.2289 43.7839 54.579ZM4.44326 100H27.5315C30.0178 100 31.9691 98.4383 31.9691 96.4483V72.8461C31.9691 69.5061 26.7321 68.0149 23.9751 70.6446L0.886871 94.2418C-1.32879 96.6599 0.88687 100 4.44326 100Z"
+                  fill="#5C5CFF"
+                />
+              </svg>
             </li>
-            <li className='flex justify-center'> 
-              <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <svg 
+            <li className="flex justify-center">
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <svg
                   className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
@@ -51,11 +72,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                 </svg>
-                
               </a>
             </li>
-            <li className='flex justify-center'>
-              <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <li className="flex justify-center">
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                   aria-hidden="true"
@@ -67,8 +90,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 </svg>
               </a>
             </li>
-            <li className='flex justify-center'>
-              <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <li className="flex justify-center">
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                   aria-hidden="true"
@@ -84,10 +110,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </div>
       </aside>
 
-      <div>
-      <section>{children}</section>
+      <div className="">
+        <section className=" h-screen w-auto bg-gray-300 z-0">
+          {children}
+        </section>
       </div>
-      
     </>
   );
 }

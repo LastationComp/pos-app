90caed8142f7993ab06805e15c26a34b203389d4
BagusFary <bagusfaryananta@gmail.com>
2023-11-26 23:11:48 +0700

    update

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     const getUpdatePage = () => {
         const dashboard = '/dashboard/'
         if (path == dashboard + 'members') return 'Members'
-
+        if (path == dashboard + 'products') return "Products"
         return 'Dashboard'
     }
   return (
